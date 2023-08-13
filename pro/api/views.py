44b9c3a3e735671287d8b608c3c20597fca7d51a
najimpatel <59526180+najimpatel@users.noreply.github.com>
2023-08-13 09:07:06 +0530

    Update views.py

--- pro/api/views.py
+++ pro/api/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render
-# from rest_framework import generics
-
 from .models import Note
 from .serializers import NoteSerializer
 from rest_framework import viewsets 
 
-# Create your views here.
+
 class NoteView(viewsets.ModelViewSet):
     queryset = Note.objects.all()
     serializer_class = NoteSerializer
