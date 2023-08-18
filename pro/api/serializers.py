5122e50aefac649b00c76f6a535e65f61f629eba
najimpatel <59526180+najimpatel@users.noreply.github.com>
2023-08-18 21:04:01 +0530

    Update serializers.py

--- pro/api/serializers.py
+++ pro/api/serializers.py
@@ -3,4 +3,4 @@ from .models import Note
 class Noteserializer(serializers.Modelserializer):
     class Meta:
         model = Note
-        fields = ('id', 'title', 'text')
+        fields = '__all__'
